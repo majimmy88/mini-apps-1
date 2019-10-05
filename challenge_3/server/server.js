ac18f31ac4398b9976c335aae73696912c560745
Jimmy Ma <jimmyma@Jimmys-MacBook-Pro.local>
2019-10-05 14:46:27 -0700

    App renders, working on checkout features

--- challenge_3/server/server.js
+++ challenge_3/server/server.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const path = require('path');
+const controller = require('./controllers/controller.js')
 
+
+const app = express()
+const PORT = 8000
+
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(PORT, ()=> console.log(`Checkout app listening on port ${PORT}!`))
 
